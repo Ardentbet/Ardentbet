5429726726980a182961a5067638a54d5755e74f
Ardentbet <yaroslav2097@gmail.com>
2026-06-06 17:42:00 +0500

    fix layout css

--- yes/app/layout.tsx
+++ yes/app/layout.tsx
@@ -1,6 +1,8 @@
+﻿import type { Metadata } from "next";
+import "./globals.css";
 import Script from "next/script";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Ardentbet",
   description: "Telegram Mini App",
 };
@@ -11,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <head />
       <body>
         <Script
